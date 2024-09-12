6f6939eceeaf914f53ea32d8084131e65564373d
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2024-09-12 13:07:35 +0100

    added home and about HTML pages which can be rendered through flask

--- run.py
+++ run.py
@@ -3,13 +3,19 @@ Introduction to Flask project
 '''
 
 # Importing the os module from the standard Python library
+
 import os
 # Importing the Flask class from the flask module
-from flask import Flask
+
+from flask import Flask, render_template
 
 # Creating an instance of the Flask class and storing it in a variable called app
 # The first argument of the Flask class is the name of the application’s module - our package
-# __name__ is a built-in Python variable. It is needed so that Flask knows where to look for templates, static files
+# __name__ is a built-in Python variable. It is needed so that Flask knows where to look
+# for templates and static files.
+# render_template is a function that comes with the Flask class. It allows you to use
+# HTML files in your Python code.
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,12 +23,10 @@ def index():
     '''
     Returns HTML
     '''
-    return '<h1>Hello,</h1> <h2>World<h2>'
+    return render_template('index.html')
 
-# __main__ is the name of the default module in Python
-if __name__ == '__main__':
+if __name__ == '__main__': # __main__ is the name of the default module in Python
     app.run(
         host=os.environ.get('IP', '0.0.0.0'),
-        port=int(os.environ.get('PORT', 5000)),
+        port=int(os.environ.get('PORT', '5000')),
         debug=True)
-    
