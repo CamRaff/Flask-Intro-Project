24f80e7761cc0a15fde7e3c467c3179e7f39b49d
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2024-09-18 09:54:37 +0100

    introduction of a further html page for data display

--- run.py
+++ run.py
@@ -35,9 +35,23 @@ def about(): # View to be called by the Jinja function for the about page
     Returns the about page
     '''
     data = []
-    with open("data/company.json", "r") as json_data:
+    with open("data/company.json", "r", encoding='utf-8') as json_data:
         data = json.load(json_data)
     return render_template('about.html', page_title='About', company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    '''
+    Returns info based on the url key
+    '''
+    member = {}
+    with open("data/company.json", "r", encoding='utf-8') as json_data:
+        data = json.load(json_data)
+        for obj in data: 
+            if obj["url"] == member_name:
+                member = obj
+    return render_template('member.html', member=member)
 
 
 @app.route('/contact')
