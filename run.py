f6a2abc5a8da14b4d42beebd10642f5cdf2984db
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2024-09-19 13:49:26 +0100

    addition of flash messages

--- run.py
+++ run.py
@@ -10,7 +10,10 @@ import os
 import json
 # Importing the json module from the standard Python library
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+
+if os.path.exists("env.py"):
+    import env
 
 # Creating an instance of the Flask class and storing it in a variable called app
 # The first argument of the Flask class is the name of the application’s module - our package
@@ -20,6 +23,7 @@ from flask import Flask, render_template
 # HTML files in your Python code.
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route('/')
 def index(): # View to be called by the Jinja function for the index/home page
@@ -54,11 +58,15 @@ def about_member(member_name):
     return render_template('member.html', member=member)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact(): # View to be called by the Jinja function for the contact page
     '''
     Returns the contact page
     '''
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(request.form.get("name")))
+        # print(request.form.get("name"))
+        # print(request.form["email"]) # This is a more streamlined version of the above
     return render_template('contact.html', page_title='Contact')
 
 
